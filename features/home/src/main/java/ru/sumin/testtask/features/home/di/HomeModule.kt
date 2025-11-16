083a3a45d323e12f05565f58e14f1fb406187bb7
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-16 22:38:27 +0300

    feat(home): implement presentation layer (viewmodel, state, and UI)

---
+++ features/home/src/main/java/ru/sumin/testtask/features/home/di/HomeModule.kt
@@ -0,0 +1,24 @@
+package ru.sumin.testtask.features.home.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.sumin.testtask.features.home.data.repository.CoursesRepositoryImpl
+import ru.sumin.testtask.features.home.domain.repository.CoursesRepository
+import ru.sumin.testtask.features.home.domain.usecase.GetCoursesUseCase
+import ru.sumin.testtask.features.home.presentation.HomeViewModel
+
+val homeModule = module {
+
+    single<CoursesRepository> {
+        CoursesRepositoryImpl(api = get())
+    }
+
+    factory { GetCoursesUseCase(repository = get()) }
+
+
+    viewModel {
+        HomeViewModel(
+            getCoursesUseCase = get(),
+        )
+    }
+}

db699618e98fdec7eeab0f377d4c2960e7bd8199
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-16 14:31:49 +0300

    feat(app): initialize Koin and connect network module in Application class

---
+++ app/src/main/java/ru/sumin/testtask/CoursesApp.kt
@@ -0,0 +1,21 @@
+package ru.sumin.testtask
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import ru.sumin.testtask.core.network.di.networkModule
+
+class CoursesApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@CoursesApp)
+            modules(listOf(networkModule))
+        }
+    }
+}

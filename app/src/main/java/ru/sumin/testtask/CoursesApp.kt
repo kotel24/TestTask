bcf5703a2d8cc9644d2d5ccde2d7a16d94dc0154
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-16 16:09:24 +0300

    feat(app): connect auth feature and apply app theme

--- app/src/main/java/ru/sumin/testtask/CoursesApp.kt
+++ app/src/main/java/ru/sumin/testtask/CoursesApp.kt
@@ -6,6 +6,7 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import ru.sumin.testtask.core.network.di.networkModule
+import ru.sumin.testtask.features.auth.di.authModule
 
 class CoursesApp : Application() {
 
@@ -15,7 +16,7 @@ class CoursesApp : Application() {
         startKoin {
             androidLogger(Level.DEBUG)
             androidContext(this@CoursesApp)
-            modules(listOf(networkModule))
+            modules(listOf(networkModule, authModule))
         }
     }
 }

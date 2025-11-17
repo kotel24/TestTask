570434fbef02c03ff36d67d27ea33d50bc67b480
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-11-17 22:19:29 +0300

    feat(app): connect favorites Koin module and navigation

--- features/profile/src/main/java/ru/sumin/testtask/features/profile/presentation/ProfileScreen.kt
+++ features/profile/src/main/java/ru/sumin/testtask/features/profile/presentation/ProfileScreen.kt
@@ -1,16 +1,18 @@
 package ru.sumin.testtask.features.profile.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import ru.sumin.testtask.core.ui.theme.AppBlack
 
 @Composable
 fun ProfileScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(AppBlack),
         contentAlignment = Alignment.Center
     ) {
         Text(text = "Экран Аккаунта")
